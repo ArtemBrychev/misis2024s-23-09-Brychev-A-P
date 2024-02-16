328f9c9634533621b3ce691ed80dba296f1dab25
Artem Brychev <chosen_one05@mail.ru>
2024-02-16 23:16:31 +0300

    Made some class operators

--- prj.lab/complex/complex.cpp
+++ prj.lab/complex/complex.cpp
@@ -1,22 +1,61 @@
 // 2022 by Polevoi Dmitry under Unlicense
 #include <complex/complex.hpp>
+#include <cmath>
 
 Complex Complex::operator-() const noexcept { return Complex(-re, -im); }
 
-bool Complex::operator==(const Complex& rhs) const noexcept { return true; }
-bool Complex::operator!=(const Complex& rhs) const noexcept { return true; }
+bool Complex::operator==(const Complex& rhs) const noexcept {
+	if (this->re == rhs.re and this->im == rhs.im) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+bool Complex::operator!=(const Complex& rhs) const noexcept {
+	if (this->re == rhs.re and this->im == rhs.im) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
 
-Complex& Complex::operator+=(const Complex& rhs) noexcept { return *this; }
+Complex& Complex::operator+=(const Complex& rhs) noexcept {
+	this->re  = this->re  + rhs.re;
+	this->im = this->im + rhs.im;
+	return *this;
+}
 Complex& Complex::operator+=(const double rhs) noexcept { return operator+=(Complex(rhs)); }
 
-Complex& Complex::operator-=(const Complex& rhs) noexcept { return *this; }
+Complex& Complex::operator-=(const Complex& rhs) noexcept { 
+	this->re = this->re - rhs.re;
+	this->im = this->im - rhs.im;
+	return *this;
+}
 Complex& Complex::operator-=(const double rhs) noexcept { return operator-=(Complex(rhs)); }
 
-Complex& Complex::operator*=(const Complex& rhs) noexcept {  return *this; }
-Complex& Complex::operator*=(const double rhs) noexcept { return *this; }
+Complex& Complex::operator*=(const Complex& rhs) noexcept {
+	this->re  = this->re * rhs.re - this->im * rhs.im;
+	this->im = this->re * rhs.im + this->im * rhs.re;
+	return *this;
+}
+Complex& Complex::operator*=(const double rhs) noexcept {
+	this->re = this->re * rhs;
+	this->im = this->im * rhs;
+	return *this;
+}
 
-Complex& Complex::operator/=(const Complex& rhs) { return *this; }
-Complex& Complex::operator/=(const double rhs) { return *this; }
+Complex& Complex::operator/=(const Complex& rhs) {
+	this->re = (this->re * rhs.re + this->im * rhs.im) / (pow(rhs.re, 2) + pow(rhs.im, 2));
+	this->im = (this->im * rhs.re - this->re * rhs.im) / (pow(rhs.re, 2) + pow(rhs.im, 2));
+	return *this;
+}
+Complex& Complex::operator/=(const double rhs) {
+	this->re = this->re / rhs;
+	this->im = this->im / rhs;
+	return *this;
+}
 
 std::ostream& Complex::WriteTo(std::ostream& ostrm) const noexcept { return ostrm; }
 std::istream& Complex::ReadFrom(std::istream& istrm) noexcept { return istrm; }
