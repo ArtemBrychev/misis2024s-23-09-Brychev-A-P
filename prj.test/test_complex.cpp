44f7586fe9699ba8b716daa22ffacdb4a5690876
Artem Brychev <chosen_one05@mail.ru>
2024-02-27 19:06:01 +0300

    Changed something

--- prj.test/test_complex.cpp
+++ prj.test/test_complex.cpp
@@ -1,6 +1,12 @@
 // 2022 by Polevoi Dmitry under Unlicense
 #include <complex/complex.hpp>
+#include <iostream>
 
 int main() {
-  Complex c;
+  Complex c1(23, 4);
+  Complex c2(45, 12);
+  Complex c3 = c1 / c2;
+  Complex c4 = c1 * c2;
+  std::cout << c3.re << ' ' << c3.im << std::endl;
+  std::cout << c4.re << ' ' << c4.im;
 }
