2c237d75d2f1efffbcec10944bb89a0b1b645395
Artem Brychev <chosen_one05@mail.ru>
2024-04-08 19:39:39 +0300

    Sheeeet

--- prj.lab/StackArrT/StackArrT.hpp
+++ prj.lab/StackArrT/StackArrT.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef STACKARRT_HPP
 #define STACKARRT_HPP
+#include <iostream>
 #include <cstddef>
 #include <stdexcept>
 
@@ -11,7 +12,7 @@ public:
 	StackArrT();
 	~StackArrT();
 	StackArrT(std::ptrdiff_t rhs);//
-	StackArrT(const StackArrT<T>& other);
+	StackArrT(const StackArrT<T>& other);//
 	StackArrT(StackArrT<T>&& other);
 	///StackArrT(const std::initializer_list<T>& list);
 
@@ -21,13 +22,13 @@ public:
 	void swap(StackArrT<T>& other);
 	void merge(StackArrT<T>& other);
 
-	bool empty() const;
+	bool empty() const;//
 	std::ptrdiff_t size() const;//
 
-	bool operator==(const StackArrT<T>& rhs) const;
-	bool operator!=(const StackArrT<T>& rhs) const;
+	bool operator==(const StackArrT<T>& rhs) const;//
+	bool operator!=(const StackArrT<T>& rhs) const;//
 
-	StackArrT<T>& operator=(const StackArrT<T>& rhs) noexcept;
+	StackArrT<T>& operator=(const StackArrT<T>& rhs) noexcept;//
 	StackArrT<T>& operator=(StackArrT<T>&& other);
 private:
 	std::ptrdiff_t size_ = 0;   //!< число элементов в буфере
@@ -38,7 +39,16 @@ private:
 template <typename T>
 StackArrT<T>::StackArrT(std::ptrdiff_t rhs) {
 	this->size_ = rhs;
-	this->data_ = new T[rhs];
+	this->data_ = new T[rhs]; 
+}
+
+template <typename T>
+StackArrT<T>::StackArrT(const StackArrT<T>& other) {
+	this->size_ = other.size();
+	this->data_ = new T[other.size()];
+	for (int i = 0; i <= other.i_top_; ++i) {
+		this->push(other.data_[i]);
+	}
 }
 
 template <typename T>
@@ -74,5 +84,71 @@ void StackArrT<T>::pop() {
 	i_top_ -= 1;
 }
 
+template <typename T>
+bool StackArrT<T>::empty() const{
+	if (i_top_ == -1) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template <typename T>
+StackArrT<T>& StackArrT<T>::operator=(const StackArrT<T>& rhs) noexcept {
+	if (this->empty() == true) {
+		for (int i = 0; i <= rhs.i_top_; ++i) {
+			this->push(rhs.data_[i]);
+		}
+		return *this;
+	}
+	else if (this->size() == rhs.size()) {
+		for (int i = 0; i <= rhs.i_top_; ++i) {
+			std::cout << data_[i] << ' ' << rhs.data_[i] << std::endl;;
+			this->data_[i] = rhs.data_[i];
+		}
+		this->i_top_ = rhs.i_top_;
+		return *this;
+	}
+}
+
+template <typename T>
+void StackArrT<T>::merge(StackArrT<T>& other) {
+	StackArrT<T> temp(this->size() + other.size());
+}
+
+template <typename T>
+bool StackArrT<T>::operator==(const StackArrT<T>& other) const {
+	if (this->i_top_ == other.i_top_) {
+		for (int i = 0; i <= i_top_; i++) {
+			if (data_[i] != other.data_[i]) {
+				return false;
+			}
+		}
+	}
+	else {
+		return false;
+	}
+	return true;
+}
+
+template <typename T>
+bool StackArrT<T>::operator!=(const StackArrT<T>& other) const {
+	if (*this == other) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+template <typename T>
+void StackArrT<T>::swap(StackArrT<T>& other) {
+	T* temp = new T[i_top_];
+	for (int i = 0; i <= i_top_; i++) {
+		temp[i] = data_[i];
+	}
+
+}
 
 #endif
--- prj.test/test_StackArrT.cpp
+++ prj.test/test_StackArrT.cpp
@@ -1,30 +1,43 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <iostream>
+//#include "doctest.h"
 #include <StackArrT/StackArrT.hpp>
 
 int main() {
-	StackArrT<int> Stack(4);
-	std::cout << Stack.size() << std::endl;
-	Stack.push(5);
-	std::cout << Stack.top() << std::endl;
-	int h = 7;
-	Stack.push(h);
-	std::cout << Stack.top() << std::endl;
-	Stack.push(5);
-	std::cout << Stack.top() << std::endl;
-	Stack.push(34);
-	std::cout << Stack.top() << std::endl;
-	Stack.pop();
-	std::cout << Stack.top() << std::endl;
-	std::cout << "-------------------------" << std::endl;
-	StackArrT<int> Stack2(4);
-	std::cout << Stack2.size() << std::endl;
-	Stack2.push(5);
-	std::cout << Stack2.top() << std::endl;
-	int j = 7;
-	Stack2.push(j);
-	Stack2.push(5);
-	Stack2.push(34);
-	std::cout << "-------------------------" << std::endl;
-	Stack.merge(Stack2);
-	std::cout << Stack.size() << std::endl;
+	StackArrT<int> a(4);
+	StackArrT<std::string> b(3);
+	b.push("Hello, world!");
+	b.push("by by");
+	b.pop();
+	StackArrT<std::string> h(3);
+	h = b;
+	StackArrT<std::string> j(3);
+	j.push("This can go fuck itself");
+	j.push("Sorry for bad words");
+	j.swap(h);
+	std::cout << "Last try " << j.top();
+	
 }
+
+//TEST_CASE("Creating stacks") {
+//	StackArrT<int> a(4);
+//	CHECK(4 == a.size());
+//	CHECK(a.empty() == true);
+//	StackArrT<std::string> b(3);
+//	CHECK(3 == b.size());
+//	CHECK(b.empty() == true);
+//	b.push("Hello, world!");
+//	CHECK(b.top() == "Hello, world!");
+//	CHECK(b.empty() == false);
+//	b.push("by by");
+//	b.pop();
+//	CHECK(b.top() == "Hello, world!");
+//	StackArrT<std::string> h(3);
+//	h = b;
+//	CHECK(h.top() == "Hello, world!");
+//	StackArrT<std::string> j(3);
+//	j.push("This can go fuck itself");
+//	j.push("Sorry for bad words");
+//	j = h;
+// 	CHECK(j.top() == "Hello, world!");
+//}
