5ca45f46b8231de6a1579c776409c323103ec023
Artem Brychev <chosen_one05@mail.ru>
2024-05-18 01:29:42 +0300

    Added a bit more tests

--- prj.test/test_HashTable.cpp
+++ prj.test/test_HashTable.cpp
@@ -66,3 +66,86 @@ TEST_CASE("Border patrol(Don't pay attention to it)") {
 	}
 	//std::cout << "Number of " << c;
 }
+
+TEST_CASE("Adding and returning keys and values(I am in thongs edition)") {
+	hashTable<std::string, std::string> table;
+	for (int i = 0; i < 46; ++i) {
+		std::string key = "key";
+		std::string value = "value";
+		key += std::to_string(i);
+		value += std::to_string(i);
+		table.insert(key, value);
+	}
+	for (int i = 0; i < 46; ++i) {
+		std::string key = "key";
+		std::string value = "value";
+		key += std::to_string(i);
+		value += std::to_string(i);
+		CHECK(table.contains(key) == true);
+		CHECK(table[key] == value);
+	}
+	for (auto x : table) {
+		CHECK(table[x.first] == x.second);
+	}
+}
+
+TEST_CASE("Adding and returning keys and values(I am in thongs edition vol.2)") {
+	hashTable<std::string, std::string> table;
+	for (int i = 0; i < 5; ++i) {
+		std::string key = "key";
+		std::string value = "value";
+		key += std::to_string(i);
+		value += std::to_string(i);
+		table.insert(key, value);
+	}
+	for (int i = 0; i < 5; ++i) {
+		std::string key = "key";
+		std::string value = "value";
+		key += std::to_string(i);
+		value += std::to_string(i);
+		CHECK(table.contains(key) == true);
+		CHECK(table[key] == value);
+	}
+	for (auto x : table) {
+		CHECK(table[x.first] == x.second);
+	}
+}
+
+TEST_CASE("Adding and returning values (I want a double cheeseburger)") {
+	hashTable<double, double> table;
+	double i(0.6), j(57.5);
+	while (i < 57.6 && j>0.5) {
+		table.insert(i, j);
+		++i;
+		--j;
+	}
+	i = 0.6;
+	j = 57.5;
+	while (i < 57.6 && j>0.5) {
+		CHECK(table.contains(i) == true);
+		++i;
+		--j;
+	}
+	for (auto x : table) {
+		CHECK(table[x.first] == x.second);
+	}
+
+	hashTable<double, double> table2;
+	i = 0.6;
+	j = 5.5;
+	while (i < 5.6 && j>0.5) {
+		table2.insert(i, j);
+		++i;
+		--j;
+	}
+	i = 0.6;
+	j = 5.5;
+	while (i < 5.6 && j>0.5) {
+		CHECK(table2.contains(i) == true);
+		++i;
+		--j;
+	}
+	for (auto x : table2) {
+		CHECK(table2[x.first] == x.second);
+	}
+}
