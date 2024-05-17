54896e21a826a829e82a57f9f29dea9947fd6b14
Artem Brychev <chosen_one05@mail.ru>
2024-05-18 00:13:59 +0300

    Table in almost final stage :)

--- prj.lab/HashTable/HashTable.hpp
+++ prj.lab/HashTable/HashTable.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <functional>
 #include <stdexcept>
+#include <utility>
 
 template<typename KeyType, typename ValueType>
 class hashTable {
@@ -20,6 +21,11 @@ private:
     std::hash<KeyType> hash_function;
 
 public:
+    hashTable() {
+        for (int i = 0; i < 16; ++i) {
+            table.push_back(nullptr);
+        }
+    }
 
     void insert(const KeyType& key, const ValueType& value);
     bool contains(const KeyType& key) const;
@@ -32,48 +38,106 @@ public:
             const std::vector<Node*>& table;
 
         public:
-            Iterator(size_t i, Node* node, const std::vector<Node*>& tbl);
-            Iterator& operator++();
-            bool operator!=(const Iterator& other) const;
-            std::pair<const KeyType&, ValueType&> operator*() const;
+            Iterator(size_t i, Node* node, const std::vector<Node*>& tbl) :index(i), current(node), table(tbl) {};
 
+            Iterator& operator++() {
+                if (current!=nullptr && current->next != nullptr) {
+                    current = current->next;
+                }
+                //else if(current!=nullptr && current->next == nullptr){
+                //    continue;
+                //}
+                else{
+                    size_t j(index + 1);
+                    while (table[j] == nullptr && j<table.size() - 1) {
+                        ++j;
+                    }
+                    index = j;
+                    current = table[index];
+                    //current = table[++index];
+                }
+                //std::cout << current << std::endl;
+                return *this;
+            }
 
-            Iterator begin() const;
-            Iterator end() const;
-    };
+            bool operator!=(const Iterator& other) const {
+                return (current != other.current && index!=other.index);
+            }
+
+            std::pair<const KeyType&, ValueType&> operator*() const {
+                if (current != nullptr) {
+                    std::pair<const KeyType&, ValueType&> key_value_bundle(current->key, current->value);
+                    return key_value_bundle;
+                }
+                else {
+                    throw(std::invalid_argument("Unknown error :( "));
+                }
+            }
+        };
+
+        Iterator begin() const {
+            int first = 0;
+            while (table[first] == nullptr) {
+                ++first;
+            }
+            if (first != 0) {
+                return Iterator(first, table[first], table);
+            }
+            else {
+                return Iterator(0, table[0], table);
+            }
+        }
+        Iterator end() const {
+            int last = 14;
+            while (table[last] == nullptr) {
+                --last;
+            }
+            if (last != 14) {
+                return Iterator(15, table[last+1], table);
+            }
+            else {
+                return Iterator(15, table[15], table);
+            }
+        }
+
 };
 
 template <typename KeyType, typename ValueType>
 void hashTable<KeyType, ValueType>::insert(const KeyType& key, const ValueType& value) {
-    if (table.size() == 0) {
-        for (int i = 0; i < 15; ++i) {
-            table.push_back(nullptr);
+    if (contains(key) == false) {
+        size_t index = hash_function(key) % 15;
+        if (table[index] == nullptr) {
+            Node* insertation = new Node(key, value);
+            table[index] = insertation;
         }
-    }
-    std::size_t index = hash_function(key) % 15;
-    if (table[index] == nullptr) {
-        Node* temp = new Node(key,  value);
-        table[index] = temp;
+        else {
+            Node* temp = table[index];
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            Node* insertation = new Node(key, value);
+            temp->next = insertation;
+        }
     }
     else {
-        Node* temp = table[index]->next;
-        while (temp->next != nullptr) {
-            temp = temp->next;
-        }
-        Node* adding = new Node(key,  value);
-        temp->next = adding;
+        throw(std::invalid_argument("Inserting a value with exsiting key"));
     }
 }
 
 template <typename KeyType, typename ValueType>
 bool hashTable<KeyType, ValueType>::contains(const KeyType& key) const{
-    std::size_t index = hash_function(key) % 15;
-    Node* temp = table[index];
-    if (temp != nullptr) {
-        while (temp->key != key && temp->next != nullptr) {
-            temp = temp->next;
+    size_t index = hash_function(key) % 15;
+    if (table[index] != nullptr) {
+        Node* temp = table[index];
+        while (temp->next != nullptr) {
+            if (temp->key == key) {
+                return true;
+            }
+            else {
+                temp = temp->next;
+            }
         }
-        if (temp->key == key) {
+        if(temp->key == key){
             return true;
         }
         else {
@@ -99,3 +163,4 @@ ValueType& hashTable<KeyType, ValueType>::operator[](const KeyType& key) {
         throw(std::invalid_argument("Returning value of non-existing key"));
     }
 }
+
--- prj.test/test_HashTable.cpp
+++ prj.test/test_HashTable.cpp
@@ -1,10 +1,68 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <iostream>
+#include <vector>
+#include "doctest.h"
 #include "HashTable/HashTable.hpp"
 
-int main() {
-	hashTable<int, int> g;
-	g.insert(5, 6);
-	g.insert(4, 8);
-	std::cout << g.contains(5) << " " << g.contains(4) << " " << g.contains(45) << std::endl;
-	std::cout << g[4] << " " << g[5];
+TEST_CASE("Adding and returning keys and values(int, int edition)") {
+	hashTable<int, int> table;
+	int i(0), j(57);
+	while (i < 57 && j>0) {
+		table.insert(i, j);
+		++i;
+		--j;
+	}
+	i = 0;
+	j = 57;
+	while (i < 57 && j>0) {
+		CHECK(table.contains(i) == true);
+		++i;
+		--j;
+	}
+	for (auto x : table) {
+		CHECK(table[x.first] == x.second);
+	}
+
+	hashTable<int, int> table2;
+	i = 0;
+	j = 5;
+	while (i < 5 && j>0) {
+		table2.insert(i, j);
+		++i;
+		--j;
+	}
+	i = 0;
+	j = 5;
+	while (i < 5 && j>0) {
+		CHECK(table2.contains(i) == true);
+		++i;
+		--j;
+	}
+	for (auto x : table2) {
+		CHECK(table2[x.first] == x.second);
+	}
+}
+
+TEST_CASE("Border patrol(Don't pay attention to it)") {
+	hashTable<int, int> table;
+	std::vector<int> borderline = { 4, 11, 34, 49, 30, 33, 52 };
+	int i(0), j(57);
+	while (i < 57 && j>0) {
+		if (std::find(borderline.begin(), borderline.end(), i) == borderline.end()) {
+			table.insert(i, j);
+		}
+		++i;
+		--j;
+	}
+	i = 0;
+	j = 57;
+	//std::cout << " " << std::endl << "---------------------" << std::endl << " " << std::endl;
+	int c = 0;
+	for (auto x : table) {
+		CHECK(table.contains(x.first) == true);
+		CHECK(table[x.first] == x.second);
+		//c++;
+		//std::cout << x.first << ' ' << x.second << std::endl;
+	}
+	//std::cout << "Number of " << c;
 }
